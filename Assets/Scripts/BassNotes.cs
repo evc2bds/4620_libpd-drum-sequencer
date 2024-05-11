c664165fb38e918e8811220703a670a71bd473af
evc2bds <91164928+evc2bds@users.noreply.github.com>
2024-05-10 23:06:53 -0400

    done

--- Assets/Scripts/BassNotes.cs
+++ Assets/Scripts/BassNotes.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BassNoteCapsules : MonoBehaviour
+public class BassNotes : MonoBehaviour
 {
     public LibPdInstance patch;
 
